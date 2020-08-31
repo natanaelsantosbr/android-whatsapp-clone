9e75cf2b599a776e90771775e5c1e9b6edb3c2b5
Natanael <natanael@timipro.com.br>
2020-08-31 20:04:08 -0300

    Criado interface de cadastro

--- app/src/main/java/br/natanael/android/whatsapp/CadastroActivity.java
+++ app/src/main/java/br/natanael/android/whatsapp/CadastroActivity.java
@@ -6,15 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_cadastro);
     }
 
-    public void abrirTelaDeCadastro(View view){
-        startActivity(new Intent(LoginActivity.this, CadastroActivity.class));
-    }
+
 }
