18e83deee1280ba7c704e947a171c3856cbcf850
Natanael <natanael@timipro.com.br>
2020-09-01 23:21:55 -0300

    Configurado abas

--- app/src/main/java/br/natanael/android/whatsapp/activity/MainActivity.java
+++ app/src/main/java/br/natanael/android/whatsapp/activity/MainActivity.java
@@ -3,6 +3,7 @@ package br.natanael.android.whatsapp.activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,9 +11,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import br.natanael.android.whatsapp.R;
 import br.natanael.android.whatsapp.config.ConfiguracaoFirebase;
+import br.natanael.android.whatsapp.fragment.ContatosFragment;
+import br.natanael.android.whatsapp.fragment.ConversasFragment;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +35,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Whatsapp");
         setSupportActionBar(toolbar);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Conversas", ConversasFragment.class)
+                        .add("Contatos", ContatosFragment.class)
+                .create()
+        );
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = findViewById(R.id.viewPagerTab);
+        viewPagerTab.setViewPager(viewPager);
+
+
+
     }
 
     @Override
---
+++ app/src/main/java/br/natanael/android/whatsapp/fragment/ContatosFragment.java
@@ -0,0 +1,32 @@
+package br.natanael.android.whatsapp.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import br.natanael.android.whatsapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContatosFragment extends Fragment {
+
+
+    public ContatosFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contatos, container, false);
+    }
+
+}
