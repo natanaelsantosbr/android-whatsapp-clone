3ba050a23062b864b6cc078e825b89a9ee326c7a
Natanael <natanael@timipro.com.br>
2020-09-10 00:05:27 -0300

    Listar Contato

--- app/src/main/java/br/natanael/android/whatsapp/activity/fragment/ContatosFragment.java
+++ app/src/main/java/br/natanael/android/whatsapp/activity/fragment/ContatosFragment.java
@@ -3,18 +3,39 @@ package br.natanael.android.whatsapp.activity.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import br.natanael.android.whatsapp.R;
+import br.natanael.android.whatsapp.activity.ConfiguracoesActivity;
+import br.natanael.android.whatsapp.adapter.ContatosAdapter;
+import br.natanael.android.whatsapp.aplicacao.config.ConfiguracaoFirebase;
+import br.natanael.android.whatsapp.aplicacao.model.usuarios.ModeloDeCadastroDeUsuario;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ContatosFragment extends Fragment {
+
+    private RecyclerView recyclerViewListaContatos;
+    private ContatosAdapter adapter;
+    private ArrayList<ModeloDeCadastroDeUsuario> listaContatos = new ArrayList<>();
+    private DatabaseReference usuariosRef;
+    private ValueEventListener valueEventListenerContatos;
 
 
     public ContatosFragment() {
@@ -25,8 +46,58 @@ public class ContatosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_contatos, container, false);
+
+        //Configuracoes iniciais
+        recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
+
+        usuariosRef = ConfiguracaoFirebase.getDatabaseReference().child("usuarios");
+
+        //configurar o adapter
+        adapter = new ContatosAdapter(listaContatos, getActivity());
+
+        //configurar recyclerview
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerViewListaContatos.setLayoutManager(layoutManager);
+        recyclerViewListaContatos.setHasFixedSize(true);
+        recyclerViewListaContatos.setAdapter(adapter);
+
+        return view;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerContatos);
+    }
+
+    public void recuperarContatos(){
+       valueEventListenerContatos =  usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dados: dataSnapshot.getChildren())
+                {
+                    ModeloDeCadastroDeUsuario usuario = dados.getValue(ModeloDeCadastroDeUsuario.class);
+                    listaContatos.add(usuario);
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
 
 }
---
+++ app/src/main/java/br/natanael/android/whatsapp/adapter/ContatosAdapter.java
@@ -0,0 +1,77 @@
+package br.natanael.android.whatsapp.adapter;
+
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.natanael.android.whatsapp.R;
+import br.natanael.android.whatsapp.aplicacao.model.usuarios.ModeloDeCadastroDeUsuario;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContatosAdapter extends RecyclerView.Adapter<ContatosAdapter.MyViewHolder> {
+
+    private List<ModeloDeCadastroDeUsuario> contatos;
+    private Context context;
+
+
+    public ContatosAdapter(List<ModeloDeCadastroDeUsuario> listaContatos, Context c) {
+        this.contatos  = listaContatos;
+        this.context = c;
+    }
+
+    //Puxa o layout xml
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemLista = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_contatos, parent, false);
+        return  new MyViewHolder(itemLista);
+    }
+
+    //Preenche os dados
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        ModeloDeCadastroDeUsuario usuario = contatos.get(position);
+
+        holder.nome.setText(usuario.getNome());
+        holder.email.setText(usuario.getEmail());
+
+        if(usuario.getFoto() != null)
+        {
+            Uri uri = Uri.parse(usuario.getFoto());
+            Glide.with(context).load(uri).into(holder.foto);
+        }
+        else
+            holder.foto.setImageResource(R.drawable.padrao);
+    }
+
+    @Override
+    public int getItemCount() {
+        return contatos.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        CircleImageView foto;
+        TextView nome, email;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            foto = itemView.findViewById(R.id.imageViewFotoContato);
+            nome = itemView.findViewById(R.id.textNomeContato);
+            email = itemView.findViewById(R.id.textEmailContato);
+        }
+    }
+}
