51a05e2a57194f6eca8d3acc1529161a1951937d
Natanael <natanael@timipro.com.br>
2020-09-19 01:31:33 -0300

    Listar contatos no grupo

--- app/src/main/java/br/natanael/android/whatsapp/activity/GrupoActivity.java
+++ app/src/main/java/br/natanael/android/whatsapp/activity/GrupoActivity.java
@@ -4,15 +4,37 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.natanael.android.whatsapp.R;
+import br.natanael.android.whatsapp.adapter.ContatosAdapter;
+import br.natanael.android.whatsapp.aplicacao.config.ConfiguracaoFirebase;
+import br.natanael.android.whatsapp.aplicacao.helper.UsuarioFirebase;
+import br.natanael.android.whatsapp.aplicacao.model.usuarios.ModeloDeCadastroDeUsuario;
 
 public class GrupoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerMembrosSelecionados, recyclerMembros;
+    private ContatosAdapter contatosAdapter;
+    private List<ModeloDeCadastroDeUsuario> listaMembros = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private DatabaseReference usuariosRef;
+    private FirebaseUser usuarioAtual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +51,69 @@ public class GrupoActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //Botao Voltar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+        recyclerMembros = findViewById(R.id.recyclerMembros);
+
+        //configurar o adapter
+        contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
+
+        usuariosRef = ConfiguracaoFirebase.getDatabaseReference().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
+        //configurar recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMembros.setLayoutManager(layoutManager);
+        recyclerMembros.setHasFixedSize(true);
+        recyclerMembros.setAdapter(contatosAdapter);
     }
 
+    public void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerMembros);
+    }
+
+    public void recuperarContatos(){
+        listaMembros = new ArrayList<>();
+        ModeloDeCadastroDeUsuario itemGrupo = new ModeloDeCadastroDeUsuario();
+        itemGrupo.setNome("Novo grupo");
+        itemGrupo.setEmail("");
+        listaMembros.add(itemGrupo);
+
+        contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
+        recyclerMembros.setAdapter(contatosAdapter);
+        contatosAdapter.notifyDataSetChanged();
+
+        valueEventListenerMembros =  usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot dados: dataSnapshot.getChildren())
+                {
+                    ModeloDeCadastroDeUsuario usuario = dados.getValue(ModeloDeCadastroDeUsuario.class);
+
+                    if(!usuarioAtual.getEmail().equals(usuario.getEmail()))
+                    {
+                        listaMembros.add(usuario);
+                    }
+                }
+
+                contatosAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 }
